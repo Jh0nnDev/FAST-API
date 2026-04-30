2586eb083dde96866fd2d40077107c86b02b2a7a
Jh0nnDev <joaosouzzaoliveira@gmail.com>
2026-04-30 13:41:32 -0300

    add variaveis de ambiente .env

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from database import SessionLocal
+import models
+
+app = FastAPI()
+
+class Tarefa(BaseModel):
+    id: int
+    titulo_tarefa: str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/tarefas", response_model=list[Tarefa])
+def buscar_tarefas(db: Session = Depends(get_db)):
+    tarefas = db.query(models.Tarefa).all()
+    return tarefas
+
+@app.post("/tarefas", response_model = Tarefa)
+def criar_tarefa(tarefa: Tarefa, db: Session = Depends(get_db)):
+    nova_tarefa = models.Tarefa(titulo_tarefa=tarefa.titulo_tarefa)
+    db.add(nova_tarefa)
+    db.commit()
+    return nova_tarefa 
+
